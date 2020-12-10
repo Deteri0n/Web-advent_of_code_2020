18bbd19858cd2f38ad58e3ed475275e81f2188c7
Etienne LC <etlecoent@gmail.com>
2020-12-10 18:35:18 -0500

    Solved challgenge 1

---
+++ day2/challenge1.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const checkPasswords = (parsedData) => {
+  let nbValidPasswords = 0;
+  for (let array of parsedData) {
+    const lowerLim = parseInt(array[0], 10);
+    const upperLim = parseInt(array[1], 10);
+    const string = array[3];
+    const regex = new RegExp(array[2], 'g');
+    const matchedChar = string.match(regex) || [];
+    if (lowerLim <= matchedChar.length && matchedChar.length <= upperLim) {
+      nbValidPasswords ++;
+    }
+  }
+  console.log(nbValidPasswords);
+}
+
+fs.readFile('./puzzleInput.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  const arrayData = data.split(/[-: '\n']+/);
+  const parsedData = arrayData.map((e, i) => { 
+    return i % 4 === 0 ? arrayData.slice(i, i + 4) : null; 
+  }).filter(e => { return e; });
+
+  checkPasswords(parsedData);
+});
