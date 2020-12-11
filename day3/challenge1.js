8477e0c3ba1d54f8a963357bebe95181b9658bf5
Etienne LC <etlecoent@gmail.com>
2020-12-11 13:34:26 -0500

    Solved challenge 1

---
+++ day3/challenge1.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const checkSlop = (parsedData) => {
+  let nbTrees = 0;
+  
+  let x = 0;
+
+  for (let y of parsedData) {
+    if (x >= y.length) {
+      x = x - y.length;
+    }
+    if (y.charAt(x) === '#') {
+      nbTrees ++;
+    }
+    x = x + 3;
+  }
+  console.log(nbTrees);
+}
+
+fs.readFile('./puzzleInput.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  const arrayData = data.split(/['\n']+/);
+  checkSlop(arrayData);
+});
