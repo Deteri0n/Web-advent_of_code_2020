82cb7709c4c5e8586a52c8d1350273a196d2c5a8
Etienne LC <etlecoent@gmail.com>
2020-12-17 15:44:36 -0500

    Solved challenge 1

--- day6/challenge1.js
+++ day6/challenge1.js
@@ -1,10 +1,32 @@
 const { readFile } = require('fs');
 
+const getGroupAnswers = (groupe) => {
+  // Creating the object holding the answers
+  const form = {};
+  for (var i = 0; i<26; i++) {
+    form[String.fromCharCode(97 + i)] = false;
+  }
+  
+  for (let answer in form) {
+    let i = 0;
+    while (!form[answer] && i < groupe.length) {
+      groupe[i].includes(answer) ? form[answer] = true : i++; 
+    }
+  }
+  return form;
+}
 
+const countGroupeAnswers = (parsedData) => {
+  
+  const nbAnswersArray = parsedData.map(groupe => Object.values(getGroupAnswers(groupe)).filter((v) => v === true).length)
+  
+  let nbAnswers = nbAnswersArray.reduce((a,b) => a + b);
+  console.log(nbAnswers);
+}
 
 readFile('./puzzleInput.txt', 'utf8', (err, data) => {
   if (err) throw err;
   const arrayData = data.split('\n\n');
   const parsedData = arrayData.map((e) => e.split(/[ '\n']+/));
-  console.log(parsedData);
+  countGroupeAnswers(parsedData);
 });
